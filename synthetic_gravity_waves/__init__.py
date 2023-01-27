bfaa6c1da4a08422fb0d5d81272ec334e0d3bb30
Leif Denby <leif@denby.eu>
2023-01-27 17:40:13 +0000

    bump version to v0.1.2 (#6)

--- synthetic_gravity_waves/__init__.py
+++ synthetic_gravity_waves/__init__.py
@@ -2,4 +2,4 @@ from .components import make_2d_wavepacket, make_carrier_wave_2d, make_gaussian_
 from .composite import make_synthetic_gravity_wave_composite
 from .point_sampling import modified_poisson_disk_sampling
 
-__version__ = "0.1.0"
+__version__ = "0.1.2"
